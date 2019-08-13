ec7e1390ee71ffbf46464e7248e61f4260679863
Nicolas VERGNE <nivergne@e1r9p19.42.fr>
2019-08-13 05:36:34 +0200

    add doc fill_rooms

--- srcs/parse/fill_rooms.c
+++ srcs/parse/fill_rooms.c
@@ -6,14 +6,14 @@
 /*   By: nivergne <nivergne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/01 02:39:11 by qgirard           #+#    #+#             */
-/*   Updated: 2019/08/13 05:09:50 by nivergne         ###   ########.fr       */
+/*   Updated: 2019/08/13 05:35:46 by nivergne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
 #include "libft.h"
 
-int	fill_rooms(t_room **rooms, t_norme *norme)
+int		fill_rooms(t_room **rooms, t_norme *norme)
 {
 	t_room	*tmp;
 	t_room	*new;
@@ -42,9 +42,17 @@ int	fill_rooms(t_room **rooms, t_norme *norme)
 
 /* ==================== fill_rooms ====================
 ** fill_rooms() append the room in norme->line at then end of t_room struct
-** go to the end of the list
-** allocate a new t_room
+** tmp if the last elem of the list
+** new is the new elem we add to the list
+** go to the end of the list and allocate a new t_room
 ** set t_room->start to 1 if it's the starting room 
 ** set t_room->end to 1 if it's the ending room 
-** 
+** if var >= 3 it means we either had start follow by end
+** at the next line or end follow by start so we put
+** var at 1 if we expect the next line to be start, 
+** var at 2 if we expect it to be end
+** var at 0 if we dont have a start or end awaiting
+** put the name of the room in t_room->name
+** if it's the fist room, room = new
+** if it's not the first room, room = tmp-next (to append the room after the last one)
 */
