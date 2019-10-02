ee4fa3935f70127baf2d80a2cc93e1dcd447c00f
nicovrg <nicolasvergne88@gmail.com>
2019-10-02 19:31:19 +0200

    push to work at school

--- src/visu/display.py
+++ src/visu/display.py
@@ -9,8 +9,6 @@ background_color = '#15202b'
 default_node_color = '#282828'
 ant_colors_list = ['#B61515', '#B66615', '#B6B615', '#66B615', '#15B615', '#15B666', '#15B6B6', '#1515B6', '#6615B6', '#B615B6', '#B61566']
 
-
-
 def create_ants(farm, graph, dict_nodes_pos, num_steps):
     list_ant = []
     for num in range (1, int(farm.ants) + 1):
@@ -29,9 +27,11 @@ def display(farm):
     graph.add_edges_from(farm.links)
     dict_nodes_pos = nx.spring_layout(graph, dim = 2, k = None, pos = None, fixed = None, iterations = 50, weight = 'weight', scale = 1.0)
     
-    num_steps = 10
+    num_steps = 5
     list_ant = create_ants(farm, graph, dict_nodes_pos, num_steps)
-    # fig = plt.figure()
+    fig = plt.figure()
+    
+
     # fig.set_facecolor("#15202b")
     # nx.draw(graph, with_labels = True)
     # plt.show()
@@ -45,7 +45,7 @@ def display(farm):
 # create a dict of positions of each node in the graph object, key is name and store tuples of x, y
 # call create ant
 #   we create a list of ants
-#   for each ant we give it a number, path, location, journey and color
+#   for each ant we give it a number, path, location, journey and color (check doc in class_ant.py for more info)
 #   then we return that list
-# 
-# 
+# we then build a figure with matplotlib plt.figure (it's like the top level component of the window)
+
